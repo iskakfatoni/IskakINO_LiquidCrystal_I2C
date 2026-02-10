4072a40aeaf6adadd5ba4756e2270e4ca54a3776
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-10 17:54:44 +0700

    Add documentation comments to SetAddress example
    Added comments to explain the purpose of the example.

--- examples/SetAddress_Manual_I2C_Address/SetAddress_Manual_I2C_Address.ino
+++ examples/SetAddress_Manual_I2C_Address/SetAddress_Manual_I2C_Address.ino
@@ -1,3 +1,18 @@
+/*
+  SetAddress.ino - Manual I2C address example
+
+  Demonstrates how to manually set the I2C address
+  instead of using automatic address scanning.
+
+  Useful when the LCD I2C address is already known
+  (commonly 0x27 or 0x3F).
+
+  Library  : IskakINO_LiquidCrystal_I2C
+  Version  : 1.0.0
+  Author   : Iskak Fatoni
+  License  : MIT
+*/
+
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(16, 2);

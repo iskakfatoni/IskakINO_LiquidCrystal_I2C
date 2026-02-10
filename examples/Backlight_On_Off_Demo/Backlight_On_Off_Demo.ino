387040737487bd0f7616f27ab90f005b44d31d07
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-10 17:55:05 +0700

    Add comments to Backlight control example
    Added header comments to explain the backlight control example.

--- examples/Backlight_On_Off_Demo/Backlight_On_Off_Demo.ino
+++ examples/Backlight_On_Off_Demo/Backlight_On_Off_Demo.ino
@@ -1,3 +1,15 @@
+/*
+  Backlight.ino - Backlight control example
+
+  Demonstrates how to turn the LCD backlight ON and OFF
+  using backlight() and noBacklight() functions.
+
+  Library  : IskakINO_LiquidCrystal_I2C
+  Version  : 1.0.0
+  Author   : Iskak Fatoni
+  License  : MIT
+*/
+
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(16, 2);

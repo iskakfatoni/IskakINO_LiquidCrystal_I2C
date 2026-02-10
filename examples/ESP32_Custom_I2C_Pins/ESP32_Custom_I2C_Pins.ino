715861581dcf0a77b65a467aa7255bd26cfbbdac
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-10 17:56:00 +0700

    Add documentation comments to ESP32 I2C example
    Added header comments to provide example usage and author information.

--- examples/ESP32_Custom_I2C_Pins/ESP32_Custom_I2C_Pins.ino
+++ examples/ESP32_Custom_I2C_Pins/ESP32_Custom_I2C_Pins.ino
@@ -1,3 +1,19 @@
+/*
+  ESP32_I2C_CustomPins.ino - ESP32 custom I2C pins example
+
+  Demonstrates how to use custom SDA and SCL pins
+  on ESP32 by initializing Wire manually.
+
+  Default example:
+    SDA = GPIO 4
+    SCL = GPIO 5
+
+  Library  : IskakINO_LiquidCrystal_I2C
+  Version  : 1.0.0
+  Author   : Iskak Fatoni
+  License  : MIT
+*/
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 

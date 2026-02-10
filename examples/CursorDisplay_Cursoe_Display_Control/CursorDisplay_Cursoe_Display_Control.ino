be59345424b4327bec26f8989ba484b168006e85
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-10 17:55:30 +0700

    Add header comment for CursorDisplay example
    Added a header comment to describe the example and its functionality.

--- examples/CursorDisplay_Cursoe_Display_Control/CursorDisplay_Cursoe_Display_Control.ino
+++ examples/CursorDisplay_Cursoe_Display_Control/CursorDisplay_Cursoe_Display_Control.ino
@@ -1,3 +1,15 @@
+/*
+  CursorDisplay.ino - Display control example
+
+  Demonstrates display() and noDisplay() functions
+  to control LCD visibility without clearing content.
+
+  Library  : IskakINO_LiquidCrystal_I2C
+  Version  : 1.0.0
+  Author   : Iskak Fatoni
+  License  : MIT
+*/
+
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(16, 2);
